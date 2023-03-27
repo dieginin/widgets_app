04df536ff08ed4c1fdbfe5f7c804ed3c81b20120
Diego Balestra <diegobalestra9@gmail.com>
2023-03-26 21:20:29 -0500

    Removed TODO

--- lib/core/screens/buttons/buttons_screen.dart
+++ lib/core/screens/buttons/buttons_screen.dart
@@ -45,10 +45,7 @@ class _ButtonView extends StatelessWidget {
             OutlinedButton.icon(onPressed: () {}, icon: const Icon(Icons.terminal), label: const Text('Ourline Icon')),
             TextButton(onPressed: () {}, child: const Text('Text')),
             TextButton.icon(onPressed: () {}, icon: const Icon(Icons.account_box_outlined), label: const Text('Text Icon')),
-
-            // TODO Custom button
             const CustomButton(),
-
             IconButton(onPressed: () {}, icon: const Icon(Icons.app_registration_rounded)),
             IconButton(
               onPressed: () {},
