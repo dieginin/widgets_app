fd0f7c9bc2a2e95f3d7355f9f50086f68b224adc
Diego Balestra <diegobalestra9@gmail.com>
2023-03-27 11:55:22 -0500

    Title change

--- lib/main.dart
+++ lib/main.dart
@@ -12,7 +12,7 @@ class MainApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp.router(
       routerConfig: appRouter,
-      title: 'Widgets App',
+      title: 'Flutter Widgets',
       debugShowCheckedModeBanner: false,
       theme: AppTheme(selectedColor: 2).getTheme(),
     );
