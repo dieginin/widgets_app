5d0705e8e7eb575bd07e1f65535e1c099c1eea49
Diego Balestra <diegobalestra9@gmail.com>
2023-03-27 22:43:34 -0500

    Drawer finished

--- lib/core/screens/home/home_screen.dart
+++ lib/core/screens/home/home_screen.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
 
 import '/config/menu/menu_items.dart';
+import '/core/widgets/shared/side_menu.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
@@ -10,11 +11,15 @@ class HomeScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final scaffoldKey = GlobalKey<ScaffoldState>();
+
     return Scaffold(
+      key: scaffoldKey,
       appBar: AppBar(
         title: const Text('Flutter + Material3'),
       ),
       body: _HomeView(),
+      drawer: SideMenu(scaffoldKey: scaffoldKey),
     );
   }
 }
