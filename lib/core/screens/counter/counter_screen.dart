919a41782df7fea67edc2f754b429450a4ff6615
Diego Balestra <diegobalestra9@gmail.com>
2023-03-27 23:38:58 -0500

    Counter provider finished

---
+++ lib/core/screens/counter/counter_screen.dart
@@ -0,0 +1,39 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+import '/core/providers/counter_provider.dart';
+import '/core/providers/theme_provider.dart';
+
+class CounterScreen extends ConsumerWidget {
+  const CounterScreen({super.key});
+
+  static const name = 'counter_screen';
+
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    final int clickCounter = ref.watch(counterProvider);
+    final bool isDarkmode = ref.watch(isDarkmodeProvider);
+
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Counter Screen'),
+        actions: [
+          IconButton(
+            onPressed: () => ref.read(isDarkmodeProvider.notifier).update((state) => !state),
+            icon: Icon(isDarkmode ? Icons.dark_mode_outlined : Icons.light_mode_outlined),
+          )
+        ],
+      ),
+      body: Center(
+        child: Text(
+          'Valor: $clickCounter',
+          style: Theme.of(context).textTheme.titleLarge,
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () => ref.read(counterProvider.notifier).state++,
+        child: const Icon(Icons.add),
+      ),
+    );
+  }
+}
