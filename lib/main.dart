915ee0be0b9b158fe39a363a40200b98bd99f2d5
Diego Balestra <diegobalestra9@gmail.com>
2023-03-27 23:36:09 -0500

    Riverpod setup

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,12 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 import 'config/router/app_router.dart';
 import 'config/theme/app_theme.dart';
 
-void main() => runApp(const MainApp());
+void main() => runApp(
+      const ProviderScope(child: MainApp()),
+    );
 
 class MainApp extends StatelessWidget {
   const MainApp({super.key});
